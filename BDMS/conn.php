4ffedb39913d929c3b4c704a1322e3ec48a434bb
Archanaadevi <archanaa3124@gmail.com>
2024-12-07 18:36:16 +0530

    Add files via upload

---
+++ BDMS/conn.php
@@ -0,0 +1,4 @@
+<?php
+//$conn=mysqli_connect("localhost","root","","blood_bank_database") or die("Connection error");
+$conn=mysqli_connect("blood-bank.c5w4eee441zc.eu-north-1.rds.amazonaws.com","admin","BloodBank123","blood_bank_database") or die("Connection error");
+?>
